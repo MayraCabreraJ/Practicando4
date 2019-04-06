60d93d284bb2c1841fc9c3d4f32400fa33f1d24d
Mayra Cabrera <mayracabrerajaldin@gmail.com>
2019-04-05 23:18:55 -0400

    Commit con Modelo MVC

---
+++ MVC3/MVC3/Models/Products.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVC3.Models
+{
+    public enum UniType
+    {
+        Botella,
+        Litro,
+        Galon,
+        Metro,
+        Tonelada,
+        Kg
+    }
+    public enum StatusType
+    { Activo,
+      Inactivo
+    }
+    public class Products
+    {
+        [Key]
+        public int ProductId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        [Required]
+        public UniType Unit{ get; set; }
+        public StatusType Status { get; set; }
+    }
+}
